64d8e4c23f9a86229bfaa668feda1f9b9b9d29bf
Ari-Pekka Repo <aprepo@gmail.com>
2024-10-20 23:44:02 +0300

    Show connections

--- main.go
+++ main.go
@@ -3,26 +3,46 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
+	"strings"
+
+	gopsutil "github.com/shirou/gopsutil/v3/net"
 )
 
-func main() {
-	fmt.Println("Test")
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		log.Fatal("Failed to get network interfaces", err)
-		return
+func isLocalAddr(addr string) bool {
+	privateRanges := []string{
+		"10.",
+		"127.",
+		"172.",
+		"192.168.",
 	}
 
-	for _, iface := range interfaces {
-		fmt.Printf("Interface: %v\n", iface.Name)
-		addresses, err := iface.Addrs()
-		if err != nil {
-			log.Fatal("Failed to get addresses", err)
-			continue
+	for _, privateRange := range privateRanges {
+		if strings.HasPrefix(addr, privateRange) {
+			return true
 		}
-		for _, addr := range addresses {
-			fmt.Printf(" Address: %v\n", addr.String())
+	}
+	return false
+}
+
+func main() {
+	stats, err := gopsutil.IOCounters(true)
+	if err != nil {
+		log.Fatal("Failed to get IOCounters")
+		return
+	}
+	for _, stat := range stats {
+		fmt.Printf("Interface: %s Sent: %d Recv: %d\n", stat.Name, stat.BytesSent, stat.BytesRecv)
+	}
+
+	conns, err := gopsutil.Connections("all")
+	if err != nil {
+		log.Fatal("Error getting connections")
+	}
+	for _, con := range conns {
+		var localIP string = con.Laddr.IP
+		var remoteIP string = con.Raddr.IP
+		if !isLocalAddr(remoteIP) {
+			fmt.Printf("Connected from: %v:%v to: %v:%v STATE: %v\n", localIP, con.Laddr.Port, remoteIP, con.Raddr.Port, con.Status)
 		}
 	}
 }
